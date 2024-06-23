1dd136f3b10e0e4dacda7b49a3d527474a0f93fa
Medhat Elbesy <152287116+MedhatElbesy@users.noreply.github.com>
2024-06-23 15:59:52 +0300

    add api.php file

---
+++ Backend/routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::controller(UserController::class)->group(function (){
+    Route::post('register','register');
+    Route::post('login','login');
+    Route::post('logout','logout')->middleware('AssignGuardAdmins:user-api');
+});
+
+Route::controller(AdminController::class)->group(function () {
+    Route::post('admin/login', 'login');
+    Route::post('admin/logout', 'logout')->middleware('AssignGuardAdmins:admin-api');
+});
+
+
+Route::group(['middleware' => ['auth:admin-api']], function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+
+});
+
+Route::group(['middleware' => ['auth:user-api']], function () {
+    Route::resource('orders', OrderController::class);
+    Route::get('/user/index',[ProductController::class,'index']);
+    Route::get('/user/show/{id}',[ProductController::class,'show']);
+
+});
